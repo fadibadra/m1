c82bee4a54145c1a8af53507ee96c21d78b105b5
Fadi Badra <badra@univ-paris13.fr>
2026-02-05 11:48:17 +0100

    avec vote majoritaire

--- 2025_2026/tps/seance_9_a_12_miniprojets_poo/cbp/algo/knn.py
+++ 2025_2026/tps/seance_9_a_12_miniprojets_poo/cbp/algo/knn.py
@@ -1,12 +1,7 @@
 import numpy as np 
 def vote_majoritaire(classes_des_voisins):
-    counts = {}
-    for c in classes_des_voisins:
-        if c not in counts:
-            counts[c] = 0
-        else:
-            counts[c] += 1
-    return max(counts, key=counts.get)
+    classes, counts = np.unique(classes_des_voisins,return_counts=True)
+    return classes[np.argmax(counts)]
 
 def kneighbors(x, X_cb, sim, n_neighbors=3, return_similarities=False):
     similarites = sim(x, X_cb)
